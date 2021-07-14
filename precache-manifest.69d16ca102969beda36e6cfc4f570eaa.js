fa7cbd40355b82bb72cbf42384403c713c19e707
Michael Phung <mikophg2001@gmail.com>
2021-07-14 16:20:57 -0700

    Updates

---
+++ precache-manifest.69d16ca102969beda36e6cfc4f570eaa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4d5374fad02761d177124eba0cca7f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "3293a5edd956aff2880b",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "9dea1e7ced71d0c400c1",
+    "url": "/home/static/js/2.cef436d8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.cef436d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3293a5edd956aff2880b",
+    "url": "/home/static/js/main.506932fa.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "0335f6c838395ca16fb3e42f28beab1e",
+    "url": "/home/static/media/mathnesium_logo.0335f6c8.jpeg"
+  },
+  {
+    "revision": "cd0ac7829fdf9cc2f36aecc46cad849b",
+    "url": "/home/static/media/profilepic.cd0ac782.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
